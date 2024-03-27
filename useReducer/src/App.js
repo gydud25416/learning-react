315337c0fdd94fea0c3d8c9d4a9655b9d33aa3f6
gydud25416 <gydud25416@gmail.com>
2024-03-27 09:51:07 +0900

    Update App.js

--- useReducer/src/App.js
+++ useReducer/src/App.js
@@ -1,57 +1,34 @@
  import Header from './component/Header'
  import TodoEditor from './component/TodoEditor';
  import TodoList from './component/TodoList';
- import TestComp from './component/TestComp';
- import { useRef, useState } from 'react';
+ import { useRef, useReducer } from 'react';
 import './App.css';
 
-const mockTodo =[
-  {
-    id:0, 
-    isDone: false,
-    content: "React 공부하기",
-    createDate: new Date().getTime(),
-  },
-  {
-    id:1, 
-    isDone: false,
-    content: "빨래 널기",
-    createDate: new Date().getTime(),
-  },
-  {
-    id:2, 
-    isDone: false,
-    content: "노래 연습하기",
-    createDate: new Date().getTime(),
-  }
-]
+function reducer(state, action){
+
+  return state
+}
+ 
 function App() {
 
-  const [todo, setTodo] = useState(mockTodo);
+ const [todo, dispatch] = useReducer(reducer, mockTodo)
   const idRef = useRef(3);
 
   const onCreate = (content) =>{ //아이템 생성
-    const newItem ={
-      id: idRef.current,
-      content,
-      idDone: false,
-      createDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
-    idRef.current ++;
+     idRef.current ++;
      
   }
 
   const onUpdate = (targetId) =>{ //아이템 수정
-    setTodo(todo.map((it) => it.id === targetId ? {...it, isDone: !it.isDone } : it))
+     
   }
 
   const onDelete = (targetId) =>{
-    setTodo(todo.filter( (it) => it.id !== targetId ))
+     
   }
   return (
     <div className="App">
-      <TestComp />
+ 
       <Header /> 
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} /> 
