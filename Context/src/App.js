09362ac20ba893299be67d3092162f73686d32f6
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-04-01 18:13:51 +0900

    Context_study
    Context_study

---
+++ Context/src/App.js
@@ -0,0 +1,94 @@
+ import Header from './component/Header'
+ import TodoEditor from './component/TodoEditor';
+ import TodoList from './component/TodoList';
+ import React, { useRef, useReducer, useCallback } from 'react';
+import './App.css';
+
+export const TodoContext = React.createContext();
+
+function reducer(state, action){
+  switch(action.type){
+    case "CREATE":{
+      return [action.newItem, ...state]
+  }
+    case "UPDATE":
+      return state.map((it)=>
+        it.id === action.targetId //key 값
+        ? {
+          ...it,
+          isDone: !it.isDone,
+        }
+        : it
+      )
+    case "DELETE":
+      return state.filter((it)=> it.id !== action.targetId);
+    default:
+  return state
+  }
+}
+const mockTodo =[
+  {
+    id:0, 
+    isDone: false,
+    content: "React 공부하기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id:1, 
+    isDone: false,
+    content: "빨래 널기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id:2, 
+    isDone: false,
+    content: "노래 연습하기",
+    createDate: new Date().getTime(),
+  }
+]
+function App() {
+
+ const [todo, dispatch] = useReducer(reducer, mockTodo)
+  const idRef = useRef(3);
+
+  const onCreate = (content) =>{ //아이템 생성
+    dispatch({
+      type: "CREATE",
+      newItem:{
+        id: idRef.current,
+        content,
+        isDone: false,
+        createDate: new Date().getTime(),
+      }
+    })
+     idRef.current ++;
+     
+  }
+
+  const onUpdate = useCallback((targetId) =>{ //아이템 수정
+     dispatch({
+      type:"UPDATE",
+      targetId, //key={it.id}
+     });
+  },[]);
+
+  const onDelete = useCallback((targetId) =>{
+     dispatch({
+      type: "DELETE",
+      targetId,
+     })
+  }, []);
+  return (
+    <div className="App">
+ 
+      <Header /> 
+      <TodoContext.Provider value={{ todo, onCreate, onUpdate, onDelete }} >
+        <TodoEditor />
+        <TodoList /> 
+      </TodoContext.Provider>
+    </div>
+  );
+}
+
+
+export default App; 
---
+++ Context/src/component/TodoEditor.js
@@ -0,0 +1,39 @@
+
+import { useRef, useState, useContext } from 'react';
+import './TodoEditor.css'
+import { TodoContext } from '../App';
+
+const TodoEditor = ()=>{
+
+    const {onCreate} = useContext(TodoContext)
+
+    const [content, setContent] = useState("")
+    const inputRef = useRef();
+    const handleChange = (e)=>{
+        setContent(e.target.value)
+    }
+    const onKeyDown= (e)=>{
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    }
+    const onSubmit = ()=>{
+        if(!content){
+            alert("내용을 입력해주세요.")
+            inputRef.current.focus();
+            return
+        }
+        onCreate(content)
+        setContent("")
+    }
+    return(
+        <div className='TodoEditor'>
+            <h4>새로운 Todo 작성하기 🖋️</h4>
+            <div className='editor_wrapper'>
+                <input ref={inputRef} onChange={handleChange} onKeyDown={onKeyDown} value={content} placeholder='새로운 Todo...' />
+                <button onClick={onSubmit}>추가</button>
+            </div>
+        </div>
+    )
+}
+export default TodoEditor;
